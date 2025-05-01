eb9d40cebca537272673cfc68cab8d178ee7217d
Stadnik Andrii <stadnikandriy1@gmail.com>
2025-05-01 11:37:40 +0300

    Move to proper uv format

---
+++ src/image_localizer/__init__.py
@@ -0,0 +1,5 @@
+from .localization import localize_image
+from .netvlad import extract_netvlad
+from .retrieval import retrieve_image_matches
+
+__all__ = ["localize_image", "extract_netvlad", "retrieve_image_matches"]
