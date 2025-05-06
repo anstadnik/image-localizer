2f6e8a55457da6e32a72d931962bc66ac2602c30
Stadnik Andrii <stadnikandriy1@gmail.com>
2025-05-06 14:44:59 +0300

    Add proper main

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-from image_localizer import extract_netvlad, localize_image, retrieve_image_matches
+from image_localizer import main
 
-def main():
-    print("Extract NetVLAD features from an image:")
-    print(extract_netvlad.__doc__)
-    print()
-
-    print("Localize an image:")
-    print(localize_image.__doc__)
-    print()
-
-    print("Retrieve image matches:")
-    print(retrieve_image_matches.__doc__)
 
 if __name__ == "__main__":
     main()
--- src/image_localizer/__init__.py
+++ src/image_localizer/__init__.py
@@ -1,5 +1,6 @@
 from .localization import localize_image
 from .netvlad import extract_netvlad
 from .retrieval import retrieve_image_matches
+from .main import main
 
-__all__ = ["localize_image", "extract_netvlad", "retrieve_image_matches"]
+__all__ = ["localize_image", "extract_netvlad", "retrieve_image_matches", "main"]
